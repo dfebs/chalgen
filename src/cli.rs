0cb418a6b37021d79591ca004026348294ecbab7
Daniel Febles <daniel.febles97@gmail.com>
2023-05-27 16:41:30 -0400

    make files argument required

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[arg(short, long, num_args = 1..)]
+    #[arg(short, long, num_args = 1.., required = true)]
     pub files: Vec<String>,
 
     #[arg(short, long, default_value_t = String::from(" "))]
